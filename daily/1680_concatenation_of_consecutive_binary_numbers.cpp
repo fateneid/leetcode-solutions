c023d440a79b58caaa26672424ae8c6d4de5ef17
Faten Eid <87611899+fateneid@users.noreply.github.com>
2026-03-01 00:44:05 +0200

    leetcode: daily -> 1680

---
+++ daily/1680_concatenation_of_consecutive_binary_numbers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        long long ans = 0;
+        int bits = 0, mod = 1e9 + 7;
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0) bits++;
+            ans = ((ans << bits) + i) % mod;
+        }
+        return ans;
+    }
+};
